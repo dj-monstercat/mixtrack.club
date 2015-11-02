24283112d2a0497a1d505f48fbe9151eab6e001e
Juddus <e@juddus.com>
2015-11-02 13:48:19 +0000

    updating files

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-var mobileSupport = (function() {
-  var width = window.innerWidth,
-      site_desc = document.getElementById("site-desc");
+var mobileSupport = (function(width, site_desc) {
   if(width < 641) site_desc.removeAttribute("class");
-})();
+})(window.innerWidth, document.getElementById("site-desc"));
